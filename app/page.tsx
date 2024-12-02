72627e31fdb4f7a9329e18aec154568305925b52
Amir Shahan <amirshahan99@gmail.com>
2024-12-02 12:03:46 +0600

    Delet pmpn

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
+
+
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Amir's Blogs
@@ -13,5 +15,8 @@ export default function Page() {
         <BlogPosts />
       </div>
     </section>
+
+
+
   )
 }
